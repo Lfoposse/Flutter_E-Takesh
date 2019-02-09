ce19b9bb7688186b69c3642a8c52e0f3d9ead489
Dwilfried <wilfried@rainbowcl.net>
2019-02-09 16:50:29 +0100

    recuperation et affichage d'une liste de donnee(Services) sur le seveur

--- lib/pages/tarifs_page.dart
+++ lib/pages/tarifs_page.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:etakesh_client/model/services.dart';
+//import 'package:etakesh_client/model/services.dart';
 
-class TarifsPage extends StatelessWidget {
 
-  Future<Service> getService() async {
+class TarifsPage extends StatefulWidget {
+  @override
+  State createState() => TarifsPageState();
+}
+
+class TarifsPageState extends State<TarifsPage> {
+  final String url = "http://api.e-takesh.com:26960/api/services?access_token=ORYTcHljQHxQHbUGxBJnUjmPZ58xrjMkXBnLBncw9yiNiz3RploBX1aRGeNCP19o";
+//  final String token = "ORYTcHljQHxQHbUGxBJnUjmPZ58xrjMkXBnLBncw9yiNiz3RploBX1aRGeNCP19o";
+  List services;
+
+  @override
+  void initState(){
+    super.initState();
+    this.getService();
+  }
+
+  Future<String> getService() async {
     final response =
-    await http.get('http://api.e-takesh.com:26960/api/services/2?access_token=ORYTcHljQHxQHbUGxBJnUjmPZ58xrjMkXBnLBncw9yiNiz3RploBX1aRGeNCP19o');
+    await http.get(Uri.encodeFull(url),headers: {"Accept": "application/json"});
 
     if (response.statusCode == 200) {
       // If the call to the server was successful, parse the JSON
-      return Service.fromJson(json.decode(response.body));
+      setState(() {
+        var convertDataToJson = json.decode(response.body);
+        services = convertDataToJson;
+      });
+      return "Success";
     } else {
       // If that call was not successful, throw an error.
       throw Exception('Failed to load post');
@@ -25,91 +44,29 @@ class TarifsPage extends StatelessWidget {
         title: new Text('Tarifs des services'),
         backgroundColor: Colors.black87,
       ),
-      body: FutureBuilder<Service>(
-        future: getService(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                new Text(
-                  snapshot.data.intitule,
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 8),
-                  child: Text(
-                    snapshot.data.prix.toString(),
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
+      body: ListView.builder(
+          itemCount: services == null ? 0 : services.length,
+          itemBuilder: (BuildContext context , int index){
+            return new Center(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  new Card(
+                    child: new Container(
+                      child: new ListTile(
+                        title: Text(services[index]['intitule']),
+                        subtitle: Text(services[index]['prix'].toString()),
+                      ),
+//                      padding: const EdgeInsets.all(20.0),
                     ),
-                  ),
-                ),
-              ],
+                  )
+                ],
+              ),
             );
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
           }
-
-          // By default, show a loading spinner
-          return CircularProgressIndicator();
-        },
-      ),
-
-//      ListView.builder(
-//        itemCount: 4,
-//        itemBuilder: (context, l) {
-//          return new Column(
-//            children: <Widget>[
-//              buttonSection,
-//            ],
-//          );
-//        },
-//      ),
+      )
 
     );
   }
 
-  //Element de tarif
-
-//  Widget buttonSection = Container(
-//      child: Row(
-//          children: <Widget>[
-//            _buildItem('Taxi Course', 'Facture par heure'),
-//            _buildItem('3000 XAF', 'heure'),
-//            _buildItem('3500 XAF', 'heure'),
-//          ],
-//        ),
-//  );
-
-//  static Column _buildItem(String title, String label) {
-//    return Column(
-//      mainAxisSize: MainAxisSize.min,
-//      mainAxisAlignment: MainAxisAlignment.center,
-//      children: [
-//        new Text(
-//          title,
-//          style: TextStyle(
-//            fontSize: 22,
-//            fontWeight: FontWeight.w400,
-//          ),
-//        ),
-//        Container(
-//          margin: const EdgeInsets.only(top: 8),
-//          child: Text(
-//            label,
-//            style: TextStyle(
-//              fontSize: 12,
-//              fontWeight: FontWeight.w400,
-//            ),
-//          ),
-//        ),
-//      ],
-//    );
-//  }
 }
